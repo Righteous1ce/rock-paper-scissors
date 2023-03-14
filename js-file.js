e382e356b6ec646978f3265aee232ba631c17195
Righteous1ice <hoplabradley@gmail.com>
2023-03-14 20:39:30 +0000

    Total mess and not working at all just need a backup for a total re-do

---
+++ js-file.js
@@ -0,0 +1,18 @@
+/*
+const rockButton = document.getElementById("Rock-Button");
+    rockButton.addEventListener("click", function(){
+        const result = playRound("rock");
+        console.log(result);
+    })
+
+const paperButton= document.getElementById("Paper-Button");
+    paperButton.addEventListener("click", function(){
+        const result = playRound("paper");
+        console.log(result);
+    })
+
+const scissorsButton = document.getElementById("Scissors-Button");
+    scissorsButton.addEventListener("click", function(){
+        const result = playRound("Scissors");
+        console.log(result);
+    })*/
